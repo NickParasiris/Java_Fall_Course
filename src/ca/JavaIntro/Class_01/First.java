84c1da71e24a8cea93cd85cd41c306422980bf44
Nicholas Parasiris <nicholasparasiris@Nicholass-MBP.lan>
2021-09-16 19:06:31 -0400

    First GitHub Fall Class

---
+++ src/ca/JavaIntro/Class_01/First.java
@@ -0,0 +1,9 @@
+package ca.JavaIntro.Class_01;
+
+public class First {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+    }
+}
---
+++ src/ca/JavaIntro/Main.java
@@ -0,0 +1,8 @@
+package ca.JavaIntro;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
